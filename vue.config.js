8c9d3d29ff1de9caa912fb89b23eea78259b7d47
Tangxinming <tangxinming@csii.com.cn>
2020-01-10 13:13:26 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
-moduel.exports = {
+module.exports = {
   // 公共路径
-  publicPath: process.env.NODE_ENV === 'production'
-  ? '/circleci-demo/'
-  : '/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/circleci-demo/" : "/"
+};
